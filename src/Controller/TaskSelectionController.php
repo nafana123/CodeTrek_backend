cb2007eb048e5b5c209626066a23ee9fa8affb72
nafana123 <makartop08@mail.ru>
2024-11-16 21:06:39 +0500

    fix

--- src/Controller/TaskSelectionController.php
+++ src/Controller/TaskSelectionController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Language;
+use App\Entity\Task;
 use App\Entity\UserLanguage;
 use App\Service\TaskService;
 use App\Service\UserService;
@@ -29,29 +30,9 @@ class TaskSelectionController extends AbstractController
     #[Route("api/language/selection", name: "language_selection", methods: ["POST"])]
     public function languageSelection(Request $request)
     {
-        $user = $this->userService->getUserByToken($request);
+        $user = $this->getUser();
 
-        $data = json_decode($request->getContent(), true);
-        $lang = $this->entityManager->getRepository(Language::class)->findOneBy(['name' => $data['lang']]);
-        $activeLanguage = $this->entityManager->getRepository(UserLanguage::class)->findOneBy([
-            'user' => $user,
-            'language' => $lang
-        ]);
-        if($activeLanguage){
-            $this->entityManager->remove($activeLanguage);
-        }
-        else{
-            $activeLanguage = new UserLanguage();
-            $activeLanguage->setLanguage($lang);
-            $activeLanguage->setUser($user);
-            $this->entityManager->persist($activeLanguage);
-        }
-
-        $this->entityManager->flush();
-
-        $activeLanguages = $this->taskService->getUserLanguages($user);
-
-        return $this->json(['lang' => $activeLanguages]);
+        return $this->json(['lang' => $user]);
     }
     #[Route("api/user/languages", name: "user_languages", methods: ["GET"])]
     public function userLanguages(Request $request)
@@ -61,6 +42,41 @@ class TaskSelectionController extends AbstractController
 
         return $this->json(['lang' => $activeLanguages]);
 
+    }
+    #[Route("api/choice/tasks", name: "choice_tasks", methods: ["GET"])]
+    public function choiceTasks(Request $request)
+    {
+        $user = $this->userService->getUserByToken($request);
+
+        $activeLanguages = $this->entityManager->getRepository(UserLanguage::class)->findBy(['user' => $user]);
+
+        $languageIds = array_map(function ($userLanguage) {
+            return $userLanguage->getLanguage()->getId();
+        }, $activeLanguages);
+
+        if (empty($languageIds)) {
+            return $this->json(['tasks' => []]);
+        }
+
+        $tasks = $this->entityManager->getRepository(Task::class)->findBy([
+            'language' => $languageIds
+        ]);
+
+        if (empty($tasks)) {
+            return $this->json(['tasks' => []]);
+        }
+
+        $taskData = array_map(function ($task) {
+            return [
+                'taskId' => $task->getTaskId(),
+                'title' => $task->getTitle(),
+                'description' => $task->getDescription(),
+                'difficultyLevel' => $task->getDifficulty()->getLevel(),
+                'languageName' => $task->getLanguage()->getName(),
+            ];
+        }, $tasks);
+
+        return $this->json(['tasks' => $taskData]);
     }
 }
 
