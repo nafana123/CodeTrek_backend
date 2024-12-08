f2eb6ef86aad1c8c4de6f4408e5084f9ff9e7924
nafana123 <makartop08@mail.ru>
2024-12-08 14:40:45 +0500

    add task page

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Task;
-use App\Entity\TaskLanguage;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,8 +18,8 @@ class TaskController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    #[Route('api/task/{id}/{language}', name: 'task', methods: ['GET'])]
-    public function displayTask(string $id, Request $request, string $language): Response
+    #[Route('/api/task/{id}/{language}', name: 'task', methods: ['GET'])]
+    public function displayTask(string $id, string $language): Response
     {
         $task = $this->entityManager->getRepository(Task::class)->find($id);
 
@@ -34,5 +33,51 @@ class TaskController extends AbstractController
             'output' => $task->getOutput(),
         ]);
     }
-}
 
+    #[Route('/api/execute/task/{id}', name: 'task_execute', methods: ['POST'])]
+    public function executeCode(string $id, Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $code = $data['code'] ?? '';
+
+        $tempFilePath = sys_get_temp_dir() . '/user_code_' . uniqid() . '.js';
+        file_put_contents($tempFilePath, $code);
+
+        $command = escapeshellcmd("node $tempFilePath");
+
+        $descriptorspec = [
+            1 => ["pipe", "w"],
+            2 => ["pipe", "w"],
+        ];
+
+        $process = proc_open($command, $descriptorspec, $pipes);
+
+        if (is_resource($process)) {
+            $output = stream_get_contents($pipes[1]);
+            $error = stream_get_contents($pipes[2]);
+
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+
+            if ($error) {
+                unlink($tempFilePath);
+                return $this->json([
+                    'success' => false,
+                    'error' => 'Синтаксическая ошибка: ' . $error,
+                ]);
+            }
+
+            unlink($tempFilePath);
+            return $this->json([
+                'success' => true,
+                'output' => $output,
+            ]);
+        }
+
+        unlink($tempFilePath);
+        return $this->json([
+            'success' => false,
+            'error' => 'Ошибка выполнения кода. Попробуйте позже.',
+        ]);
+    }
+}
